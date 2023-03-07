9ea924d28718a55df1faae0381964a5c93de8d73
ksantos16 <u121364866@byui.edu>
2023-03-07 00:11:24 -0500

    updated validator.js

--- controller/admin.js
+++ controller/admin.js
@@ -61,7 +61,11 @@ const updateUser = async (req,res,next) => {
   const userId = new ObjectId(req.params.id);
   const user = {
     firstName: req.body.firstName,
+<<<<<<< HEAD
     lastName: req.body.lastName,
+=======
+    LastName: req.body.LastName,
+>>>>>>> 79bdf0a (updated validator.js)
     birthday: req.body.birthday,
     department: req.body.department,
     hireDate: req.body.hireDate,
