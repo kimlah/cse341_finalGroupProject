1091ce9ff899d5317708b926bf4de598a9722455
kimlah <71352287+kimlah@users.noreply.github.com>
2023-03-09 20:28:10 -0800

    add swagger tags to routes

--- routes/equipment.js
+++ routes/equipment.js
@@ -5,14 +5,19 @@ const validator = require('../validate');
 
 const equipmentController = require('../controller/equipment');
 
-router.get('/', equipmentController.getAll);
-
-router.get('/:id', equipmentController.getSingle);
-
+// #swagger.tags = ['Equipment']
 router.post('/', validator.validatedEquipment, equipmentController.createEquipment);
 
+// #swagger.tags = ['Equipment']
+router.get('/', equipmentController.getAll);
+
+// #swagger.tags = ['Equipment']
+router.get('/:id', equipmentController.getSingle);
+
+// #swagger.tags = ['Equipment']
 router.put('/:id', validator.validatedEquipment, equipmentController.updateEquipment);
 
+// #swagger.tags = ['Equipment']
 router.delete('/:id', equipmentController.deleteEquipment);
 
 
