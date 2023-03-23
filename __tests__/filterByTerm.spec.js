556701818f573d6fd52960d34db04d5b21f49ec2
Thomasen007 <Thomas.elliott.nielson@gmail.com>
2023-03-22 20:29:30 -0700

    jest testing

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,11 +1,11 @@
-const filterByTerm = require("../filterByTerm");
+const filterByTerm = require('../filterByTerm');
 
 describe('Filter function', () => {
   test('it should filter by a search term (link)', () => {
     const input = [
       { id: 1, url: 'https://www.url1.dev' },
       { id: 2, url: 'https://www.url2.dev' },
-      { id: 3, url: 'https://www.link3.dev' },
+      { id: 3, url: 'https://www.link3.dev' }
     ];
 
     const output = [{ id: 3, url: 'https://www.link3.dev' }];
@@ -13,35 +13,21 @@ describe('Filter function', () => {
     expect(filterByTerm(input, 'link')).toEqual(output);
 
     expect(filterByTerm(input, 'LINK')).toEqual(output); // New test
-
-
   });
 
   test('it should filter by a search term (uRl)', () => {
-    const input = [
-      { id: 1, url: 'https://www.url1.dev' },
-      { id: 2, url: 'https://www.url2.dev' },
-      { id: 3, url: 'https://www.link3.dev' },
-    ];
+    const input = [{ id: 1, url: 'https://www.url1.dev' }];
 
-    const output = [{ id: 3, url: 'https://www.link3.dev' }];
+    const output = [{ id: 1, url: 'https://www.url1.dev' }];
 
-    expect(filterByTerm(input, 'uRl'));
-
-
+    expect(filterByTerm(input, 'uRl')).toEqual(output);
   });
 
   test('it should filter by a searchTerm is empty string ', () => {
-    const input = [
-      { id: 1, url: 'https://www.url1.dev' },
-      { id: 2, url: 'https://www.url2.dev' },
-      { id: 3, url: '' },
-    ];
+    const input = [{ id: 1, url: ' ' }];
 
-    const output = [{ id: 3, url: '' }];
+    const output = [{ id: 1, url: ' ' }];
 
-    expect(filterByTerm(input, 'link'));
-
-
+    expect(filterByTerm(input, ' ')).toEqual(output);
   });
 });
