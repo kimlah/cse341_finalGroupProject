236ca6690bd0edfdfc03f498ccc0756abcdfe29c
Thomasen007 <Thomas.elliott.nielson@gmail.com>
2023-03-23 20:49:19 -0700

    update of testing

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,4 +1,4 @@
-const filterByTerm = require('../filterByTerm');
+const filterByTerm = require('../src/filterByTerm');
 
 describe('Filter function', () => {
   test('it should filter by a search term (link)', () => {
--- __tests__/routes.spec.js
+++ __tests__/routes.spec.js
@@ -1,19 +1,35 @@
-import request from 'supertest';
-import express from 'express';
-import router from '../routes/main.js';
+const request = require('supertest');
+const express = require('express');
+const router = require('../routes/index.js')
 
 const app = new express();
 app.use('/', router);
 
-describe('Admin check', function () {
-  test('responds to /', async () => {
-    const res = await request(app).get('/');
-    expect(res.header['content-type']).toBe('text/html; charset=utf-8');
-    expect(res.statusCode).toBe(200);
-    expect(res.text).toEqual('');
-  });
+// describe('GET /users', function() {
+//   const response = request(app)
+//     .get('/workOrder')
+//     .set('Accept', 'application/json')
+//     test("Finds json file", () => {
+//     response
+//       .expect('Content-Type', /json/)
+//       .expect(200)
+//     });
+// });
+app.get('/user', function(req, res) {
+  res.status(200).json({ name: 'john' });
+});
 
-  // test('responds to /hello/:name', async () => {
+
+describe('GET /routes', function() {
+  test('responds with json', async function() {
+    const response = await request(app)
+      .get('/workOrders')
+      .set('Accept', 'application/json')
+      .expect(200)
+  });
+});
+
+  // tst('responds to /hello/:name', async () => {
   //   const res = await request(app).get('/hello/jaxnode');
   //   expect(res.header['content-type']).toBe('text/html; charset=utf-8');
   //   expect(res.statusCode).toBe(200);
@@ -25,5 +41,6 @@ describe('Admin check', function () {
   //   expect(res.header['content-type']).toBe('text/html; charset=utf-8');
   //   expect(res.statusCode).toBe(200);
   //   expect(res.text).toEqual('hello Annie!');
-  // });
-});
+  
+// });
+//});
