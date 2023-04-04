fa97f7f68df026ad56f33cba16a6553eb6d3418d
ksantos16 <u121364866@byui.edu>
2023-04-03 23:03:19 -0400

    test 3

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {requiresAuth} = require('express-openid-connect');
+// const {requiresAuth} = require('express-openid-connect');
 
-router.get('/', requiresAuth, require('./swagger'));
+// router.get('/', requiresAuth, require('./swagger'));
 router.use('/', require('./swagger'));
 router.use('/admin', require('./admin'));
 router.use('/employee', require('./employee'));
